4ee4f3b73363416789b838b3664b97db3110fa74
neha-0858 <nehakudev@gmail.com>
2026-05-14 14:36:33 +0530

    experiment 5

---
+++ experiment 5/script.js
@@ -0,0 +1,14 @@
+let employee =[];
+
+function addEmployee(){
+    let emp ={
+        name:document.getElementById("name")
+        name:document.getElementById("name")
+        name:document.getElementById("name")
+        name:document.getElementById("name")
+    }
+}
+
+function dispkayemployee(){
+    
+}
